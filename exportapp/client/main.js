2ef4eb4fcd102f56602b8ef4dd4cc96ea9401876
eddyrajomarison <eddyrajomarison@gmail.com>
2023-01-09 00:20:08 +0100

    ajout des éléments clients

---
+++ exportapp/client/main.js
@@ -0,0 +1,30 @@
+import { Template } from 'meteor/templating';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.html';
+import { Exports } from "../db/collection.js";
+
+
+Template.body.events({
+  'click .export-btn'(event, instance) {
+    event.preventDefault();
+    Meteor.call('exports.insert');
+  },
+});
+ 
+Template.list.onCreated(function bodyOnCreated() {
+   Meteor.subscribe('exports');
+});
+
+Template.list.helpers({
+  exports() {
+    console.log("main.js",  Exports.find({} ).fetch());
+    return Exports.find().fetch();  
+  },
+  progressStyle(w) { return {
+    style: `width: ${w}%;`
+    }
+  },
+  getAnimated(w) { return w ==100 ? "success" : "progress-bar-animated progress-bar-striped ";},
+});
+
+
